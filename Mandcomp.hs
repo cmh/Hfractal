d135f3758cde4b0e0f028d249a07c49913a1f443
chrisholdsworth <chrisholdsworth@gmail.com>
2009-08-08 20:46:18 +0100

    Changing parallel code
    Ignore-this: b1edb9ef9eaf96773e1e8a9c5be3da24
    Uses the more natural `using` function.
    
    darcs-hash:20090808194618-cdb54-d074a24a3ee85be3177e941393470c23bcf4bba7.gz

--- Mandcomp.hs
+++ Mandcomp.hs
@@ -3,25 +3,18 @@ module Mandcomp (compMandPoints)
   where
 
 import Control.Parallel.Strategies
-
-{-
-import Prelude hiding (map)
-import Data.List.Stream
--}
-
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
+
 import Mandstate
 
 instance (Num a) => NFData (Color3 a)
 
-maxIter = 400
-
-parMapChunk cs strat f xs = map f xs `using` parListChunk cs strat
+maxIter = 800
 chunkSize = (width * height) `div` (4 * 20) 
 
 compMandPoints :: Double -> Double -> Double -> Double -> [(Int, Int, Color3 GLdouble)]
-compMandPoints xmid ymid range cm = parMapChunk chunkSize rnf (\(x,y) -> (x, y, mandPoint 0 0.0 0.0 ((range*normx x) + xmid) ((range*normy y) + ymid) cm)) [ (x,y) | x <- [0  .. width-1], y <- [0 .. height-1] ] where
+compMandPoints xmid ymid range cm = [(x,y,mandPoint 0 0.0 0.0 ((range*normx x) + xmid) ((range*normy y) + ymid) cm) | x <- [0  .. width-1], y <- [0 .. height-1] ] `using` parListChunk chunkSize rnf where
   normx i = (fi i - fi w2) / fi w2
   normy j = (fi j - fi h2) / fi h2
 
@@ -33,7 +26,7 @@ mandPoint !n !x !y cx cy cm | n > maxIter       = Color3 0 0 0
 							| (x*x + y*y) > 4.0 = colorMand x y n cm
 							| otherwise		 = mandPoint (n+1) (x*x - y*y + cx) (2*x*y + cy) cx cy cm
 
--- Color a vertex
+-- Colour a vertex
 colorMand :: Double -> Double -> Int -> Double -> Color3 GLdouble
 colorMand x y n cm = fmap realToFrac $ Color3 r g b where
 	m = 1.0 - (logBase 2 (logBase 2 $ sqrt (x*x + y*y))) + fromIntegral n
