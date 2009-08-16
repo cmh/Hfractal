33c112586eb093c1f0ff0198c5801ffac3076bb6
chrisholdsworth <chrisholdsworth@gmail.com>
2009-08-16 02:45:34 +0100

    Longer Tests
    Ignore-this: f1b8a6b70ed6d824b17a43be335578f9
    
    darcs-hash:20090816014534-cdb54-317133223b0566d86fc58a7fa25dda0da49c5f60.gz

--- MandSpeedTest.hs
+++ MandSpeedTest.hs
@@ -1,18 +1,11 @@
 import Mandcomp
 import Data.Time.Clock
-import System.Environment
-import Control.Parallel.Strategies
-import Graphics.Rendering.OpenGL
 import Data.Array.IO
 import Mandstate
 
 main = do
 	start <- getCurrentTime
 	pixarr <- newArray (0, width*height-1) 0.0 :: IO Pix
-	(compPoints 0.002 0.8 0.05 pixarr) 
-	(compPoints 0.202 2.8 1.05 pixarr)
-	(compPoints 0.002 1.1 0.001 pixarr)
-	(compPoints 0.002 1.11 0.101 pixarr)
-	(compPoints 0.002 1.12 0.201 pixarr)
+	mapM_ (\r -> compPoints 0.002 0.8 r pixarr) [0.1, 0.15 .. 1.0]
 	end <- getCurrentTime
 	putStrLn $ show (end `diffUTCTime` start) ++ " elapsed."
