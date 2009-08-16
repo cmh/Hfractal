7a2f1445c3e3edbaa5b0abc8af9deedbd6c0fb8d
chrisholdsworth <chrisholdsworth@gmail.com>
2009-08-16 03:23:12 +0100

    Further optimisations
    Ignore-this: d43e9017752d38d1d6f4f3c265de7a21
    
    darcs-hash:20090816022312-cdb54-a8592f6f04a49ff64ca3384ab94f689276fd3ab2.gz

--- Mandcomp.hs
+++ Mandcomp.hs
@@ -14,16 +14,17 @@ instance (Num a) => NFData (Color3 a)
 -- Number of iterations to escape
 mandPoint :: Int -> Double -> Double -> Double -> Double -> Double
 mandPoint !n !x !y cx cy | n > maxIter       = 0.0
-					     | (x*x + y*y) > 4.0 = 1.0 - logBase 2 (logBase 2 (sqrt (x*x + y*y))) + fromIntegral n
-					     | otherwise		 = mandPoint (n+1) (x*x - y*y + cx) (2*x*y + cy) cx cy	
+					     | (x2 + y2) > 4.0   = 1.0 - logBase 2 (0.5 * logBase 2 (x*x + y*y)) + fromIntegral n
+					     | otherwise		 = mandPoint (n+1) (x2 - y2 + cx) (2*x*y + cy) cx cy where
+	x2 = x*x
+	y2 = y*y
 
 compPoints :: Double -> Double -> Double -> Pix -> IO ()
 compPoints xm ym rng arr = mapM_ (\(x,y) -> writePix x y arr) indicies where
-	writePix x y a = 	
-		writeArray a (x + width*y) (mandPoint 0 0.0 0.0 ((rng*normx x) + xm) ((rng*normy y) + ym))
-	normx i = (fi i - fi w2) / fi w2
-	normy j = (fi j - fi h2) / fi h2
-	fi = fromIntegral
+	writePix x y a = writeArray a (x + width*y) (mandPoint 0 0.0 0.0 cx cy) where
+		fi = fromIntegral
+		cx = rng * (fi x - fi w2) / fi w2 + xm
+		cy = rng * (fi y - fi h2) / fi h2 + ym
 
 -- Colour a vertex
 colorMand :: Double -> Double -> Color3 GLdouble
