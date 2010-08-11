e6847a3e299a304324948397c4740a6e9465bbb4
Chris Holdsworth <chrisholdsworth@gmail.com>
2010-08-11 02:18:10 +0100

    Added cardiod test

--- src/PointComp.hs
+++ src/PointComp.hs
@@ -6,9 +6,12 @@ module PointComp
 
 -- Number of iterations to escape
 mandPoint :: Double -> Double -> Int -> Double
-mandPoint cx cy mi = go 0 0.0 0.0 where
-	go !n !x !y | n > mi		   = 0.0
-				| (x2 + y2) > 4.0 = 1.0 - logBase 2 (0.5 * logBase 2 (x2 + y2)) + fromIntegral n
+mandPoint cx cy mi | inCardiod cx cy = 0.0
+				   | otherwise      = go 0 0.0 0.0 where
+	inCariod cx cy = q * (q + (cx - 0.25)) < 0.25 * cy * cy where
+		q = (cx - 0.25) * (cx - 0.25) + cy*cy
+	go !n !x !y | x2 + y2 > 4.0    = 1.0 - logBase 2 (0.5 * logBase 2 (x2 + y2)) + fromIntegral n
+				| n > mi		   = 0.0
 				| otherwise		   = go (n+1) (x2 - y2 + cx) (2.0*x*y + cy) where
-		!x2 = x*x  --This CSE saves a few cycles
-		!y2 = y*y
+		!x2 = x*x 
+		!y2 = y*y  --This CSE saves a few cycles
